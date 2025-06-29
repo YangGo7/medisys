4253aa8df5623693f198a8013ea2b9e871d2bdca
INME1 <mook970511@gmail.com>
2025-06-29 11:51:10 +0000

    logihn

--- backend/backend/medical_integration/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Alert
-
-class AlertSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Alert
-        fields = ['id', 'type', 'message', 'created_at', 'is_read']
